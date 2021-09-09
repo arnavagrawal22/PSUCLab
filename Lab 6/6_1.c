e7ff0e346f9070448522607f57b831d02a26845d
arnavagrawal22 <arnav.agrawal1@learner.manipal.edu>
2021-09-09 18:09:12 +0530

    na

---
+++ Lab 6/6_1.c
@@ -0,0 +1,11 @@
+// Arnav Agrawal
+// 200905200
+// Lab 6
+// Question 1
+// Write a program that will read the value of x and evaluate the following function
+#include<stdio.h>
+int main()
+{
+    printf("Arnav Agrawal\n");
+    printf("200905200\n");
+    printf("Section M - 20\n");
